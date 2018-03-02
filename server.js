3dc8e5cd1a30ead89c323ea9f476e373dbe79c88
Lucho Andrade <luan95_12@hotmail.com>
2018-03-02 18:24:37 +1000

    adding the projects page

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
+        pageDescription: 'These are my projects'
+    })
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
